88fd013d31af0ba1d2bca29976554eecab8fb240
Ghabriel <ghabriel.nunes@gmail.com>
2018-12-01 16:04:59 -0200

    Piece rotation unit tests

--- tetris/src/lib.rs
+++ tetris/src/lib.rs
@@ -56,6 +56,105 @@ mod tests {
         assert_grid_eq(&piece, &rotation_system, "0000/0000/1111/0000");
     }
 
+    #[test]
+    fn nintendo_rotation_j() {
+        let rotation_system = rotations::build_nintendo_rotation_system();
+
+        let mut piece = new_piece(PieceKind::J, 0);
+        assert_grid_eq(&piece, &rotation_system, "000/111/001");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "010/010/110");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "100/111/000");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "011/010/010");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "000/111/001");
+    }
+
+    #[test]
+    fn nintendo_rotation_l() {
+        let rotation_system = rotations::build_nintendo_rotation_system();
+
+        let mut piece = new_piece(PieceKind::L, 0);
+        assert_grid_eq(&piece, &rotation_system, "000/111/100");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "110/010/010");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "001/111/000");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "010/010/011");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "000/111/100");
+    }
+
+    #[test]
+    fn nintendo_rotation_o() {
+        let rotation_system = rotations::build_nintendo_rotation_system();
+
+        let mut piece = new_piece(PieceKind::O, 0);
+        assert_grid_eq(&piece, &rotation_system, "11/11");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "11/11");
+    }
+
+    #[test]
+    fn nintendo_rotation_s() {
+        let rotation_system = rotations::build_nintendo_rotation_system();
+
+        let mut piece = new_piece(PieceKind::S, 0);
+        assert_grid_eq(&piece, &rotation_system, "000/011/110");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "010/011/001");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "000/011/110");
+    }
+
+    #[test]
+    fn nintendo_rotation_t() {
+        let rotation_system = rotations::build_nintendo_rotation_system();
+
+        let mut piece = new_piece(PieceKind::T, 0);
+        assert_grid_eq(&piece, &rotation_system, "000/111/010");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "010/110/010");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "010/111/000");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "010/011/010");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "000/111/010");
+    }
+
+    #[test]
+    fn nintendo_rotation_z() {
+        let rotation_system = rotations::build_nintendo_rotation_system();
+
+        let mut piece = new_piece(PieceKind::Z, 0);
+        assert_grid_eq(&piece, &rotation_system, "000/110/011");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "001/011/010");
+
+        piece.rotate(&rotation_system);
+        assert_grid_eq(&piece, &rotation_system, "000/110/011");
+    }
+
     fn new_piece(kind: PieceKind, rotation_index: usize) -> Piece {
         Piece::new(kind, Color::Blue, rotation_index)
     }
