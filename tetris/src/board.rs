2e070a584db2df46527de8f7fd763c2285161a32
Ghabriel <ghabriel.nunes@gmail.com>
2018-12-02 13:19:17 -0200

    Board: active_piece_touches_board implementation

--- tetris/src/board.rs
+++ tetris/src/board.rs
@@ -17,6 +17,8 @@ pub struct ActivePiece {
     position: usize,
 }
 
+struct Position(usize, usize);
+
 impl Board {
     pub fn new(num_rows: usize, num_columns: usize) -> Board {
         let grid_size = num_rows * num_columns;
@@ -58,35 +60,39 @@ impl Board {
     }
 
     fn has_active_piece(&self) -> bool {
-        match self.active_piece {
-            Some(_) => true,
-            None => false,
-        }
+        self.active_piece.is_some()
     }
 
     fn active_piece_touches_board(&self, settings: &Settings) -> bool {
         if let Some(ref active_piece) = self.active_piece {
+            let Position(base_line, base_column) = self.get_active_piece_position();
+
             let piece = &active_piece.piece;
-
-            let base_position = active_piece.position;
-            let base_line = base_position / self.num_columns;
-            let base_column = base_position % self.num_columns;
-
             let piece_grid = piece.get_grid(&settings.rotation_system);
             let piece_grid_size = piece_grid.0.len();
-            let piece_grid_columns   = (piece_grid_size as f64).sqrt() as usize;
+            let piece_grid_columns = (piece_grid_size as f64).sqrt() as usize;
 
             for (reverse_index, cell) in piece_grid.0.iter().rev().enumerate() {
+                if !cell {
+                    continue;
+                }
+
                 let index = piece_grid_size - 1 - reverse_index;
 
                 let cell_grid_line = index / piece_grid_columns;
                 let cell_grid_column = index % piece_grid_columns;
-                let cell_position_offset = cell_grid_line * self.num_columns + cell_grid_column;
 
                 let cell_line = base_line + cell_grid_line;
                 let cell_column = base_column + cell_grid_column;
+                let cell_position = Position(cell_line, cell_column);
 
-                if let Some(_) = self.at(cell_line, cell_column) {
+                let line_below = self.grid_index(
+                    cell_grid_line + 1,
+                    cell_grid_column,
+                    piece_grid_columns,
+                );
+
+                if !piece_grid.0[line_below] && self.cell_touches_board(cell_position) {
                     return true;
                 }
             }
@@ -95,8 +101,37 @@ impl Board {
         false
     }
 
+    fn get_active_piece_position(&self) -> Position {
+        match self.active_piece {
+            Some(ref active_piece) => {
+                let position = active_piece.position;
+                let line = position / self.num_columns;
+                let column = position % self.num_columns;
+
+                Position(line, column)
+            },
+            None => panic!("No active piece"),
+        }
+    }
+
+    fn cell_touches_board(&self, cell_position: Position) -> bool {
+        let Position(cell_row, cell_column) = cell_position;
+
+        self.is_occupied(cell_row + 1, cell_column)
+    }
+
+    fn is_occupied(&self, row: usize, column: usize) -> bool {
+        self.at(row, column).is_some()
+    }
+
+    fn grid_index(&self, row: usize, column: usize, num_columns: usize) -> usize {
+        row * num_columns + column
+    }
+
     fn at(&self, row: usize, column: usize) -> &Option<Block> {
-        &self.grid[row * self.num_columns + column]
+        let index = self.grid_index(row, column, self.num_columns);
+
+        &self.grid[index]
     }
 
     fn materialize_active_piece(&mut self, settings: &Settings) {
