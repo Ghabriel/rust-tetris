a06bd151b75634805efe9928b5f47b09419df1cc
Ghabriel <ghabriel.nunes@gmail.com>
2018-12-21 23:26:05 -0200

    Game Loop implementation

--- tetris/src/v2/mvc/game_loop.rs
+++ tetris/src/v2/mvc/game_loop.rs
@@ -1,36 +1,57 @@
-struct GameLoop<TUpdate, TRender> {
+use std::time::{Duration, Instant};
+
+static DEFAULT_UPDATE_FREQUENCY: u8 = 25;
+
+pub struct GameLoop<TUpdate, TRender> {
     update: TUpdate,
     render: TRender,
+    running: bool,
+    update_period: f64,
 }
 
 impl<TUpdate, TRender> GameLoop<TUpdate, TRender>
 where
-    TUpdate: FnMut(GameLoop<TUpdate, TRender>),
-    TRender: FnMut(GameLoop<TUpdate, TRender>),
+    TUpdate: Fn(f64) -> bool,
+    TRender: Fn() -> bool,
 {
-    fn start() {
-        // double t = 0.0;
-        // const double dt = 0.01;
-
-        // double currentTime = hires_time_in_seconds();
-        // double accumulator = 0.0;
-
-        // while ( !quit )
-        // {
-        //     double newTime = hires_time_in_seconds();
-        //     double frameTime = newTime - currentTime;
-        //     currentTime = newTime;
-
-        //     accumulator += frameTime;
-
-        //     while ( accumulator >= dt )
-        //     {
-        //         integrate( state, t, dt );
-        //         accumulator -= dt;
-        //         t += dt;
-        //     }
-
-        //     render( state );
-        // }
+    pub fn new(update: TUpdate, render: TRender) -> GameLoop<TUpdate, TRender> {
+        GameLoop {
+            update,
+            render,
+            running: false,
+            update_period: 1000.0 / (DEFAULT_UPDATE_FREQUENCY as f64),
+        }
     }
+
+    pub fn set_update_frequency(&mut self, ticks_per_second: u8) {
+        self.update_period = 1000.0 / (ticks_per_second as f64);
+    }
+
+    pub fn start(&mut self) {
+        let mut last_measured_time = Instant::now();
+        let mut accumulator = 0.0;
+
+        while self.running {
+            let now = Instant::now();
+            let elapsed_time = duration_as_millis(now.duration_since(last_measured_time));
+            last_measured_time = now;
+            accumulator += elapsed_time as f64;
+
+            while accumulator >= self.update_period {
+                if (self.update)(self.update_period) {
+                    self.running = false;
+                }
+
+                accumulator -= self.update_period;
+            }
+
+            if (self.render)() {
+                self.running = false;
+            }
+        }
+    }
+}
+
+fn duration_as_millis(duration: Duration) -> u64 {
+    duration.as_secs() * 1000 + (duration.subsec_millis() as u64)
 }
--- tetris/src/v2/mvc/mod.rs
+++ tetris/src/v2/mvc/mod.rs
@@ -2,5 +2,6 @@ mod game_loop;
 mod model;
 mod view;
 
+pub use self::game_loop::GameLoop;
 pub use self::model::Model;
 pub use self::view::View;
