ae322366ccc41eb26fb43d5ee2416e94b016db8b
Ghabriel Calsa Nunes <gcnunes@SEF.SC.GOV.BR>
2019-01-03 12:48:50 -0200

    Block rendering improvements

--- tetris/src/v2/mvc/view.rs
+++ tetris/src/v2/mvc/view.rs
@@ -3,6 +3,8 @@ use sfml::window::{Event, Style};
 use super::Model;
 use super::traits::Render;
 
+const TILE_SIZE: usize = 18;
+
 pub struct View {
     window: RenderWindow,
     tiles_texture: Texture,
@@ -61,21 +63,26 @@ impl View {
 
     pub fn render_board(&mut self, model: &Model) {
         let mut tile_sprite = Sprite::with_texture(&self.tiles_texture);
+        let window = &mut self.window;
+        let mut row_index = 0;
 
-        let coordinates = IntRect::new(0, 0, 18, 18);
-        tile_sprite.set_texture_rect(&coordinates);
+        model.for_each_row(&mut |row| {
+            row.iter()
+                .enumerate()
+                .for_each(|(cell_index, cell)| {
+                    // if let Some(block) = cell {
+                        let tileset_coordinates = IntRect::new(0, 0, 18, 18);
+                        tile_sprite.set_texture_rect(&tileset_coordinates);
 
-        tile_sprite.set_position((0., 0.));
-        self.window.draw(&tile_sprite);
+                        let cell_coordinates = get_cell_coordinates(row_index, cell_index);
+                        tile_sprite.set_position(cell_coordinates);
 
-        tile_sprite.set_position((18., 0.));
-        self.window.draw(&tile_sprite);
+                        window.draw(&tile_sprite);
+                    // }
+                });
 
-        tile_sprite.set_position((36., 0.));
-        self.window.draw(&tile_sprite);
-
-        tile_sprite.set_position((54., 0.));
-        self.window.draw(&tile_sprite);
+            row_index += 1;
+        });
 
         // let mut num_rows = 0;
 
@@ -94,4 +101,8 @@ impl View {
     pub fn render_active_piece(&mut self, model: &Model) {
         // TODO
     }
+}
+
+fn get_cell_coordinates(row: usize, column: usize) -> (f32, f32) {
+    ((column * TILE_SIZE) as f32, (row * TILE_SIZE) as f32)
 }
